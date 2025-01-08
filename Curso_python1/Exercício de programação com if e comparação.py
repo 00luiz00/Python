3b35a78bdbabf6ad1782a416a582a4b5fb3050e5
Luiz <juniorocha99@gmail.com>
2025-01-08 03:10:15 -0300

    att

---
+++ Curso_python1/Exercício de programação com if e comparação.py
@@ -0,0 +1,10 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor ')
+
+if primeiro_valor > segundo_valor:
+    print(f'O primeiro valor {primeiro_valor} é maior' 
+          f'do segundo {segundo_valor}')
+
+else:
+    print(f'O segundo valor {segundo_valor} é maior' 
+          f'do primeiro valor {primeiro_valor}')
